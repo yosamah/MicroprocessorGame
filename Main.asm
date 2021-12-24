2e6b0b24614a4c8a1270e49d44dffe0ca8d5f740
Ahmed Mohammed Ezzat <74915712+AbdelrahmanMohamed129@users.noreply.github.com>
2021-12-24 23:05:02 +0200

    TillBorderCircle

--- Main.asm
+++ Main.asm
@@ -1,7 +1,17 @@
 ;-----------بِسْمِ اللَّـهِ الرَّحْمَـٰنِ الرَّحِيمِ------------
 
 ;-----------------MACROS-----------------
-;changing to  text mode
+
+;-------Changing to graphics mode-------
+changeGraphicsmode Macro
+
+    mov ah,0
+    mov al,13h
+    int 10h
+
+endm changeGraphicsmode
+
+;-------Changing to text mode-------
 changeTextmode Macro
 
     mov ah,0
@@ -10,7 +20,7 @@ changeTextmode Macro
 
 endm changeTextmode
 
-;---Clear Screen---
+;-------Clear Screen-------
 ClearScreen MACRO x1,y1,x2,y2,Color
 
     mov ax,0600h
@@ -23,7 +33,7 @@ ClearScreen MACRO x1,y1,x2,y2,Color
 
 endm ClearScreen
 
-;---Set cursor---
+;-------Set cursor-------
 SetCursor MACRO x,y,PageNO
 
     mov ah,2
@@ -34,7 +44,7 @@ SetCursor MACRO x,y,PageNO
 
 endm SetCursor
 
-;---Print Character---
+;-------Print Character-------
 PrintChar MACRO Type,Color,Len,PageNO
 
     mov ah,9
@@ -46,7 +56,19 @@ PrintChar MACRO Type,Color,Len,PageNO
 
 endm PrintChar
 
-;---Draw MACRO---
+;-------Print Character in graphics mode-------
+PrintCharGraphics  MACRO char,color,times
+
+    mov al.char
+    mov ah,09h
+    mov bh,0
+    mov bl,color
+    mov cx,times
+    int 10h
+    
+ENDM PrintCharGraphics
+
+;-------Draw MACRO-------
 DrawLine MACRO x,y,HorLen,VerLen,Color,PageNO,Type
 
     SetCursor x,y,PageNO
@@ -54,14 +76,14 @@ DrawLine MACRO x,y,HorLen,VerLen,Color,PageNO,Type
 
 endm DrawLine
 
-;---Print a message on the screen---
+;-------Print a message on the screen-------
 PrintMessage MACRO MyMessage 
     mov AH,9h
     lea DX,MyMessage
     int 21h
 ENDM PrintMessage
 
-;---Status Chat---
+;-------Status Chat-------
 StatusChat MACRO msg1
 
     DrawLine WindowStart,WindowEndY-2,WindowEndX,0,03h,0,'-'
@@ -71,7 +93,7 @@ StatusChat MACRO msg1
 endm StatusChat
 
 
-;---Show Mouse---
+;-------Show Mouse-------
 ShowMouse MACRO
 
     mov ax,1
@@ -79,7 +101,7 @@ ShowMouse MACRO
 
 endm ShowMouse
 
-;---Get Cursor position---
+;-------Get Cursor position-------
 GetCursorPos MACRO x,y,stat
 
     mov si,200
@@ -100,7 +122,7 @@ TheEnd2:
 
 endm GetCursorPos
 
-;---Get key no wait---
+;-------Get key no wait-------
 GetKeyNoWait MACRO SC
 
     mov al,0
@@ -118,7 +140,7 @@ GetKeyNoWait MACRO SC
 
 endm GetKeyNoWait
 
-;---Get key wait---
+;-------Get key wait-------
 GetKeyWait MACRO sc,char
 
     mov ah,0
@@ -128,14 +150,14 @@ GetKeyWait MACRO sc,char
 
 endm GetKeyWait
 
-;---Read message---
+;-------Read message-------
 ReadMessage MACRO msg
     mov ah,0Ah
     lea dx,msg
     int 21h
 endm ReadMessage
 
-;---Scroll---
+;-------Scroll-------
 Scroll MACRO x1,y1,x2,y2,Color,line
 
     mov ah,06h
@@ -242,71 +264,115 @@ invalidcharacter:
 numbercomplete: 
 ENDM ReadNumber
 
+;--------Draw pixel----------
+DrawPixel Macro row,col,color
 
-;----------------------------------------
+    mov cx,col
+    mov dx,row
+    mov al,color
+    mov ah,0ch
+    int 10h 
+
+endm DrawPixel
+
+;--------Draw line----------
+DrawLineGraphics Macro start,end,HV,flag,color
+    LOCAL vertical
+    LOCAL horizontal
+    LOCAL bara
+
+    pusha
+    mov ax,flag
+    cmp ax,0 ;If flag = 0 -> draw horizontal line
+
+    mov si,start
+    mov di,end
+
+    jne vertical
+
+    horizontal:
+    DrawPixel HV,si,color
+    inc si
+    cmp si,di
+    jbe horizontal
+
+    jmp bara
+
+    vertical:
+    DrawPixel si,HV,color
+    inc si
+    cmp si,di
+    jbe vertical
+
+    bara: popa
+
+endm DrawLineGraphics
+
+;--------Draw filled rectangle----------
+DrawFilledRectangle macro x1,y1,x2,y2,colorBorder,colorFill
+    LOCAL MyZeft
+
+    pusha
+    mov dx,x1
+    mov di,x2
+MyZeft:     
+    DrawLineGraphics y1,y2,dx,0,colorFill
+    inc dx
+    cmp dx,di
+    jbe MyZeft
+
+    DrawLineGraphics y1,y2,x1,0,colorBorder
+    DrawLineGraphics y1,y2,x2,0,colorBorder
+    DrawLineGraphics x1,x2,y1,1,colorBorder
+    DrawLineGraphics x1,x2,y2,1,colorBorder
+    
+    popa
+
+endm DrawFilledRectangle
 
 
-;-----------------MACROS-----------------
-;changing to  text mode
-changeTextmode Macro
-    mov ah,0
-    mov al,3
-    int 10h
-endm changeTextmode
-;Set cursor
-SetCursor MACRO x,y,PageNO
+;--------Draw registers----------
+DrawRegisters  MACRO x1,y1
 
-    mov ah,2
-    mov dl,x
-    mov dh,y
-    mov bh,PageNO
-    int 10h
+    DrawFilledRectangle x1,y1,x1+9,y1+36,White,Purple
+    DrawFilledRectangle x1+13,y1,x1+22,y1+36,White,Purple
+    DrawFilledRectangle x1+26,y1,x1+35,y1+36,White,Purple
+    DrawFilledRectangle x1+39,y1,x1+48,y1+36,White,Purple
 
-endm SetCursor
+ENDM DrawRegisters
 
-;Print Character
-PrintChar MACRO Type,Color,Len,PageNO
+;--------Main menu----------
+MainMenu  MACRO 
+    
+     changeTextmode  
+    ;Get Info  of user1 
+    GetUserName User1Name
+    ReadNumber IntialPoints1
+    call GetEnter
 
-    mov ah,9
-    mov bh,PageNO
-    mov al,Type
-    mov cx,Len
-    mov bl,Color
-    int 10h
+    ;Get Info  of user2 
+    GetUserName User2Name
+    ReadNumber IntialPoints2
+    call GetEnter
 
-endm PrintChar
+    
+    Call MainScreen
 
-;Draw MACRO
-DrawLine MACRO x,y,HorLen,VerLen,Color,PageNO,Type
-
-    SetCursor x,y,PageNO
-    PrintChar Type,Color,HorLen,PageNO
-
-endm DrawLine
-
-PrintMessage MACRO MyMessage ;print a message on the screen
-    mov AH,9h
-    lea DX,MyMessage
-    int 21h
-ENDM PrintMessage
+ENDM MainMenu
 
 ;----------------------------------------
 .model small
 .stack 64
 .386
-
 .data 
 
 User1                   db 'USER1$'
-    
 User1Name               DB 15,?,15 DUP('$') , '$'
 IntialPoints1           dw ?
     
 User2                   db 'USER2$'
 User2Name               DB 15,?,15 DUP('$') , '$'
-IntialPoints2           dw ?     
-    
-    
+IntialPoints2           dw ?      
     
 EnterName               db 'Please enter your name:',10, 13, '$'
     
@@ -329,6 +395,14 @@ startrow                db 2
 startcol                db 0
 endcol                  db 20
 
+;-----------Circle variables-----------
+XC                      dw 50
+YC                      dw 50
+X_Circle                dw 0
+Y_Circle                dw 0
+Radius                  dw 8
+P                       dw 0
+
 ;Geting username variables 
 MulNmber                db 10
 messageinvalidcharacter DB 'Invalid Input',10,13, '$'
@@ -346,11 +420,14 @@ ChatMessage             db 70,?,70 dup('$')
 ChatMessage2            db ?,'$'
 
 
-
 ;Global window variables
 WindowStart             equ 0
 WindowEndX              equ 80
 WindowEndY              equ 24
+
+WindowGStart            equ 0
+WindowGEndX             equ 199
+WindowGEndY             equ 319
         
 MousePosX               dw ?
 MousePosY               dw ?
@@ -361,6 +438,25 @@ F3Scancode              equ 61d
 F2Scancode              equ 60d
 F1Scancode              equ 59d
 ESCScancode             equ 1d
+
+
+;Colors
+Black                   equ 0
+Blue                    equ 1
+Green                   equ 2 
+Cyan                    equ 3
+Red                     equ 4
+Magenta                 equ 5
+Brown                   equ 6
+LightGray               equ 7
+DarkGray                equ 8
+Purple                  equ 9
+LightGreen              equ 0Ah
+LightCyan               equ 0Bh
+LightRed                equ 0Ch
+LightMagenta            equ 0Dh
+Yellow                  equ 0Eh
+White                   equ 0Fh
         
 ;----------------------------------------------
 .code
@@ -368,29 +464,28 @@ main proc far
     mov ax,@DATA
     mov ds,ax
 
-  
+    ;MainMenu
 
-
-    changeTextmode  
-    ;Get Info  of user1 
-    GetUserName User1Name
-    ReadNumber IntialPoints1
-    call GetEnter
-
-    ;Get Info  of user2 
-    GetUserName User2Name
-    ReadNumber IntialPoints2
-    call GetEnter
-
+    changeGraphicsmode
     
-    Call MainScreen
-   
+    ;DrawLineGraphics 100,200,50,1,0Fh
+    ;DrawFilledRectangle 50,50,120,150,06,0Fh
+
+    call DrawCircle
+    dec Radius
+    call DrawCircle
+
+
+    ;call GameScreen
+    
 
     mov ah,4ch ;hlt
     int 21h
 
 main endp
-;---------------------Proceduers-------------------
+
+
+;---------------------Proceduers---------------------
 
 ;-------MainScreen-------
 MainScreen proc near
@@ -437,7 +532,7 @@ MainScreen proc near
 endp MainScreen
 
 
-;----Chat Proc----
+;-------Chat Proc-------
 ChatWindow proc near
 
     ClearScreen WindowStart,WindowStart,WindowEndX,WindowEndY,0
@@ -490,7 +585,8 @@ Khalas:
     ret
 ChatWindow endp 
 
-;------Checking cursor position to scroll-------
+
+;-------Checking cursor position to scroll-------
 CheckCursor proc near
 
     cmp User1CursorY,ChatHeight-1
@@ -508,7 +604,8 @@ CheckCursor proc near
 ret
 endp CheckCursor
 
-;----Clearing screen after ENTER-----
+
+;-------Clearing screen after ENTER-------
 GetEnter Proc
     
     return: 
@@ -524,5 +621,148 @@ GetEnter Proc
     RET
 GetEnter ENDP
 
-end main
 
+;-------Game Screen-------
+GameScreen proc
+
+    DrawLineGraphics WindowGStart,WindowGEndY,WindowGStart+16,0,Purple
+    DrawLineGraphics WindowGStart,WindowGEndY,WindowGStart+78,0,Purple
+    DrawLineGraphics WindowGStart,WindowGEndY,WindowGStart+89,0,Purple
+    DrawLineGraphics WindowGStart,WindowGEndY,WindowGStart+180,0,Purple
+
+    DrawLineGraphics WindowGStart+17, WindowGStart+89 ,WindowGStart+160 ,1, Purple
+    DrawLineGraphics WindowGStart,WindowGStart+16,WindowGStart+147,1,Purple
+    DrawLineGraphics WindowGStart,WindowGStart+16,WindowGStart+173,1,Purple
+    DrawLineGraphics WindowGStart+90,WindowGStart+180,WindowGStart+150,1,Purple
+    DrawLineGraphics WindowGStart+90,WindowGStart+180,WindowGStart+170,1,Purple
+
+    DrawRegisters 24,40 ;MACROO
+    DrawRegisters 24,85
+
+    DrawRegisters 24,200 ;MACROO
+    DrawRegisters 24,245
+
+    ret
+GameScreen endp
+
+;--------Draw Circle----------
+DrawCircle proc
+
+        pusha
+
+        mov ax,Radius 
+        mov Y_Circle,ax
+
+        ;plot initial point
+        call plot
+        ;P=1-Radius
+        mov ax,01
+        mov dx,Radius
+        xor dx,0ffffh
+        inc dx
+        add ax,dx
+        mov P,ax
+
+        ;while(X_Circle<Y_Circle) 
+    loop1:  mov ax,X_Circle
+        cmp ax,Y_Circle
+        jnc stop
+
+        ;X_Circle++
+        inc X_Circle
+
+        ;if(P<0)
+        mov ax,P
+        rcl ax,01
+        jnc jump2
+
+        ;P+=2X_Circle+1
+        mov ax,X_Circle
+        rcl ax,01
+        inc ax
+        add ax,P
+        jmp jump3
+
+        ;else
+        ;Y_Circle++
+        ;P+=2(X_Circle-Y_Circle)+1;
+    jump2:  dec Y_Circle
+        mov ax,X_Circle
+        mov dx,Y_Circle
+        xor dx,0ffffh
+        inc dx
+        add ax,dx
+        rcl ax,01
+        jnc jump4
+        or ax,8000h
+    jump4: inc ax
+        add ax,P
+
+    jump3:  mov P,ax
+        ;plot point
+        call plot
+        jmp loop1
+stop:
+        mov X_Circle,0
+        mov Y_Circle,0
+        mov P,0
+        popa
+endp DrawCircle
+
+;-------Plotting circle-------
+plot proc
+    mov ah,0ch
+    mov al,02
+
+    mov cx,XC
+    add cx,X_Circle
+    mov dx,YC
+    add dx,Y_Circle
+    int 10h
+
+    mov cx,XC
+    add cx,X_Circle
+    mov dx,YC
+    sub dx,Y_Circle
+    int 10h
+
+    mov cx,XC
+    sub cx,X_Circle
+    mov dx,YC
+    add dx,Y_Circle
+    int 10h
+
+    mov cx,XC
+    sub cx,X_Circle
+    mov dx,YC
+    sub dx,Y_Circle
+    int 10h
+
+    mov cx,XC
+    add cx,Y_Circle
+    mov dx,YC
+    add dx,X_Circle
+    int 10h
+
+    mov cx,XC
+    add cx,Y_Circle
+    mov dx,YC
+    sub dx,X_Circle
+    int 10h
+
+    mov cx,XC
+    sub cx,Y_Circle
+    mov dx,YC
+    add dx,X_Circle
+    int 10h
+
+    mov cx,XC
+    sub cx,Y_Circle
+    mov dx,YC
+    sub dx,X_Circle
+    int 10h
+
+    ret
+    plot endp
+
+end main
